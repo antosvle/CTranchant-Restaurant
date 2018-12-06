ee99609ff8c1ec0aca75f6b1a706e3a019f2e45a
Anto <antoine.savalle1@viacesi.fr>
2018-12-06 14:28:52 +0100

    init visual studio

---
+++ Restaurant/Room/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
