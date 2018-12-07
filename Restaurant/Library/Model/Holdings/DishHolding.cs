c622a5018d918002edf6c39f53c1d1a17594fa8b
Maxime Mulder <maxime.mulder@viacesi.fr>
2018-12-07 10:39:04 +0100

    Update Library Model
    We're going to put a lot in this library

---
+++ Restaurant/Library/Model/Holdings/DishHolding.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace Library.Model
+{
+    class DishHolding : Holding<Dish>
+    {}
+}
---
+++ Restaurant/Library/Model/Holdings/Holding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Model
+{
+    class Holding<Holdable> where Holdable : IHoldable
+    {
+        private HashSet<Holdable> holdables;
+
+        public Holdable Give(Holdable holdable)
+        {
+            if (holdables.Contains(holdable))
+            {
+                return holdable;
+            }
+            else
+            {
+                return default(Holdable);
+            }
+        }
+
+        public void Take(Holdable holdable)
+        {
+            holdables.Add(holdable);
+        }
+    }
+}
