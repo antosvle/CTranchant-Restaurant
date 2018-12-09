1b00f439c0566620bf6be9b2ed8cc60e5a9f4484
Maxime Mulder <maxime.mulder@viacesi.fr>
2018-12-09 19:55:22 +0100

    That's a nice and functional Timeline you got there 😎😎😎😎

--- Restaurant/Kitchen/Program.cs
+++ Restaurant/Kitchen/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using Library;
 using Library.DatabaseLayer;
 using Library.Utils.DTO;
+using Library.Controller;
 
 namespace Kitchen
 {
     class Program
     {
         private static DataFactory Injector;
-        
+
+        private static EventWaitHandle wh = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
-            Injector = DataFactory.GetInstance();
+            /*Injector = DataFactory.GetInstance();
             KitchenService KitchenDB = Injector.GetKitchenService();
 
             List<IngredientDTO> listIngredient = KitchenDB.GetIngredients();
@@ -24,7 +28,21 @@ namespace Kitchen
                 Console.WriteLine(listIngredient[i].Name);
             }
             
-            Console.ReadLine();
+            Console.ReadLine();*/
+
+            Timeline.UnitDuration = 1000;
+
+            new Thread(() => {
+
+                Console.WriteLine("Yo");
+
+                Timeline.Wait(10);
+
+                Console.WriteLine("Oy");
+
+            }).Start();
+
+            Timeline.Start();
         }
     }
 }
--- Restaurant/Library/Controller/Timeline.cs
+++ Restaurant/Library/Controller/Timeline.cs
@@ -1,53 +1,85 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
-using System.Collections.Specialized;
 
 namespace Library.Controller
 {
-    class Timeline
+    public class Timeline
     {
-        private static IDictionary<long, List<ManualResetEvent>> Locks = new SortedDictionary<long, List<ManualResetEvent>>();
+        private static IDictionary<long, List<ManualResetEvent>> lockers = new SortedDictionary<long, List<ManualResetEvent>>();
 
-        private static int UnitDuration { get; set; } = 0;
+        public static int UnitDuration { get; set; } = 0;
 
-        private static long CurrentTimestamp = 0;
+        private static long CurrentTime = 0;
         
         public static void Start()
         {
             while (true)
             {
                 Thread.Sleep(UnitDuration);
+
                 Timeline.Advance();
             }
         }
 
         public static void Wait(long units)
         {
-            long timestamp = Timeline.CurrentTimestamp + units;
+            Timeline.AddLocker(Timeline.CurrentTime + units).WaitOne();
+        }
 
-            List<ManualResetEvent> threads = Timeline.Locks[timestamp];
+        private static void Advance()
+        {
+            Timeline.CurrentTime++;
 
-            if (threads == null)
+            List<ManualResetEvent> lockers = Timeline.GetLocker(Timeline.CurrentTime);
+
+            if (lockers != null)
             {
-                threads.Add(new ManualResetEvent(false));
+                foreach (ManualResetEvent locker in lockers)
+                {
+                    locker.Set();
+                }
+
+                Timeline.lockers.Remove(Timeline.CurrentTime);
+            }
+        }
+
+        private static ManualResetEvent AddLocker(long time)
+        {
+            List<ManualResetEvent> lockers = Timeline.GetLocker(time);
+
+            ManualResetEvent locker = new ManualResetEvent(false);
+
+            if (lockers != null)
+            {
+                lockers.Add(locker);
             }
             else
             {
                 List<ManualResetEvent> locks = new List<ManualResetEvent>();
 
-                locks.Add(new ManualResetEvent(false));
+                locks.Add(locker);
 
-                Locks.Add(timestamp, locks);
+                Timeline.lockers.Add(time, locks);
             }
 
-
-            Thread.CurrentThread.Resume();
-            Thread.CurrentThread.Suspend();
+            return locker;
         }
 
-        private static void Advance()
+        private static List<ManualResetEvent> GetLocker(long time)
         {
-            Timeline.CurrentTimestamp++;
+            List<ManualResetEvent> lockers;
+
+            try
+            {
+                lockers = Timeline.lockers[time];
+            }
+            catch(Exception)
+            {
+                lockers = null;
+            }
+
+            return lockers;
         }
     }
 }
