d1706563bd64a80c0e6803795f7c797e97c8aa1a
Dreiney <nicolas.vanhove@viacesi.fr>
2018-12-10 17:25:36 +0100

    append unity

---
+++ Restaurant/res/Restaurant Simulator 2018/Assets/scripts/chefDeRangPath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chefDeRangPath : MonoBehaviour {
+
+
+    public Transform[] target;
+    public float speed;
+
+    private int current =0;
+
+    private bool ascendant = true;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		if(transform.position != target[current].position)
+        {
+           transform.position = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            //GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else
+        {
+            if (current <target.Length-1 && ascendant)
+            {
+                current++;
+            }else
+            {
+                current--;
+                ascendant = false;
+                if (current == 0)
+                {
+                    ascendant = true;
+                }
+            }
+        }
+	}
+}
