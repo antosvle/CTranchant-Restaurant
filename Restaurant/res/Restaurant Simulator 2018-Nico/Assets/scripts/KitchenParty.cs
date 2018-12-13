4367620a502a25e7277c7248d8001209380f412b
PierrotHml <pierre.hamel.eure@gmail.com>
2018-12-13 19:48:34 +0100

    Ajout Library en dll de unity

--- Restaurant/res/Restaurant Simulator 2018-Nico/Assets/scripts/KitchenParty.cs
+++ Restaurant/res/Restaurant Simulator 2018-Nico/Assets/scripts/KitchenParty.cs
@@ -5,29 +5,47 @@ using System.Threading;
 using TMPro;
 using UnityEngine;
 
-public class KitchenParty: MonoBehaviour {
+public class KitchenParty: MonoBehaviour, IObserver {
 
     public float speed;
     private Vector3 target;
-    private bool isWalking = false;
+    private bool isWalking;
     private delegate void Direction();
     private Direction dir;
     public Animator animator;
+    public float waitTimer;
+    private float wait;
+    private string order;
 
     private void Start()
     {
-       ChangeDirection("LeftToRight");
+        ChangeDirection("counter");
+        wait = waitTimer;
     }
 
     private void Update()
     {
+        if (this.order != null)
+        {
+            ChangeDirection(order);
+        }
+
         if (isWalking)
         {
             dir();
-            Debug.Log("dab");
+        }
+        else
+        {
+            waitTimer -= Time.deltaTime;
+
+            if (waitTimer < 0)
+            {
+                waitTimer = wait;
+
+                WaitingOrder();
+            }
         }
     }
-    
 
     private void ChangeDirection(string state)
     {
@@ -42,37 +60,54 @@ public class KitchenParty: MonoBehaviour {
                 if (transform.position != target)
                 {
                     animator.SetBool("IsWalkingRight", true);
+                    animator.SetFloat("Speed", speed);
                     GoTo();
                 }
-                else 
+                else if (transform.position == target)
                 {
                     isWalking = false;
-                    Debug.Log("bigoune");
                     animator.SetFloat("Speed", 0f);
                     animator.SetBool("IsWalkingRight", false);
-                    //GoTo(23f, -3f);
                 }
             };
         }
-        if(state == "toward")
-        {
-            dir = delegate ()
-            {
-                transform.Translate(Vector2.up * speed * Time.deltaTime);
-            };
-        }
-        if(state == "sit")
-        {
 
+        if (state == "RightToLeft")
+        {
+            target = new Vector3(23f, -3f);
             dir = delegate ()
             {
                 if (transform.position != target)
                 {
-                    transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+                    animator.SetBool("IsWalkingLeft", true);
+                    animator.SetFloat("Speed", speed);
+                    GoTo();
                 }
-                else
+                else if (transform.position == target)
                 {
-                    target = new Vector3(-19.5f, 1.5f); //Coordonées de la table
+                    isWalking = false;
+                    animator.SetFloat("Speed", 0f);
+                    animator.SetBool("IsWalkingLeft", false);
+                }
+            };
+        }
+
+        if (state == "counter")
+        {
+            target = new Vector3(19.5f, -5f);
+            dir = delegate ()
+            {
+                if (transform.position != target)
+                {
+                    animator.SetBool("IsWalkingLeft", true);
+                    animator.SetFloat("Speed", speed);
+                    GoTo();
+                }
+                else if (transform.position == target)
+                {
+                    isWalking = false;
+                    animator.SetFloat("Speed", 0f);
+                    animator.SetBool("IsWalkingLeft", false);
                 }
             };
         }
@@ -81,5 +116,31 @@ public class KitchenParty: MonoBehaviour {
     public void GoTo() 
     {
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    public void WaitingOrder()
+    {
+        if (transform.position == new Vector3(23f, -3f))
+        {
+            ChangeDirection("LeftToRight");
+        }
+
+        if (transform.position == new Vector3(25.7f, -3f))
+        {
+            ChangeDirection("RightToLeft");
+        }
+
+        if (transform.position == new Vector3(19.5f, -5f))
+        {
+            ChangeDirection("LeftToRight");
+        }
+    }
+
+    public void OnNotify(string str)
+    {
+        if (str.Split(' ')[0] == "partyChief")
+        {
+            this.order = str.Split(' ')[1];
+        }
     }
 }
--- Restaurant/res/Restaurant Simulator 2018-Nico/Assets/scripts/MainMenu/MainMenu.cs
+++ Restaurant/res/Restaurant Simulator 2018-Nico/Assets/scripts/MainMenu/MainMenu.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Library.Utils;
 
 public class MainMenu : MonoBehaviour {
 
-	public void StartGame()
+    private void Start()
+    {
+        GlobalFactory factory = GlobalFactory.GetInstance();
+
+        TransportationService socketManager = factory.GetTransportationService(LocationEnum.IHM);
+    }
+
+    public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
     }
