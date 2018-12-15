40797a3801b65c6dc855fa2f88b3ea20c0ee1100
Anto <antoine.savalle1@viacesi.fr>
2018-12-15 12:04:35 +0100

    update ip config

--- Restaurant/Library/TransportationLayer/NetworkConfig.cs
+++ Restaurant/Library/TransportationLayer/NetworkConfig.cs
@@ -10,12 +10,12 @@ namespace Library.TransportationLayer
 
         internal static readonly string LOCAL_IP = "127.0.0.1";
 
-        internal static readonly string IP_SERVER_KITCHEN = "192.168.1.3";
+        internal static readonly string IP_SERVER_KITCHEN = "192.168.1.51";
 
-        internal static readonly string IP_SERVER_ROOM = "192.168.1.4";
+        internal static readonly string IP_SERVER_ROOM = "192.168.1.67";
 
         internal static readonly string IP_SERVER_IHM = "192.168.1.2";
 
-        internal static readonly string IP_DATABASE = "192.168.1.1";
+        internal static readonly string IP_DATABASE = "192.168.1.67";
     }
 }
