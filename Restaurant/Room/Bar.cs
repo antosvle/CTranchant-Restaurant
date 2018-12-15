abaae7d4a471c06654ef6acc4a300acbe68ff39e
Anto <antoine.savalle1@viacesi.fr>
2018-12-15 18:27:14 +0100

    final update ROOM

--- Restaurant/Room/Bar.cs
+++ Restaurant/Room/Bar.cs
@@ -49,19 +49,18 @@ namespace Room
             Table table = null;
             string[] msg = answer.Split(' ', 2);
 
-            if(msg[0] == "done")
-            {
-                foreach(Table toble in room.Tables)
-                {
-                    if(toble.Id.ToString() == msg[1])
-                    {
-                        table = toble;
-                    }
-                }
 
-                if(table != null)
-                    table.Row.Area.AddWaiterEvent(new WaiterEvent(WaiterEventEnum.mealReady, table));
+            foreach(Table toble in room.Tables)
+            {
+                if(toble.Id.ToString() == answer)
+                {
+                    table = toble;
+                }
             }
+
+            if(table != null)
+                table.Row.Area.AddWaiterEvent(new WaiterEvent(WaiterEventEnum.mealReady, table));
+            
         }
     }
 }
